1c6f6bd117b015e179c2bac8c504a639e50d7944
tiklite78 <hafida8978@gmail.com>
2026-04-30 00:00:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1115,7 +1115,7 @@ export default function App() {
   
 
      return (
-    <div style={{ fontFamily:"'Outfit','Segoe UI',sans-serif" }}>
+    <div style=...>
       <Nav page={page} setPage={p=>{ if(["messages","dashboard","mes-devis","calendrier"].includes(p)&&!user){ go("login"); return; } go(p); }} user={user} onLogout={()=>{ setUser(null); go("home"); }} unread={unread} />
 
       {page==="home" && <Home setPage={go} setMetier={setMetier} />}
